dbe1a23cea519bb1bb4882ee20d66cb65b9e8036
João Pedro <marcondesjpb@gmail.com>
2023-09-24 10:51:53 -0300

    SM

--- Sistema/Associar Aluno.py
+++ Sistema/Associar Aluno.py
@@ -1,61 +1,20 @@
 import openpyxl
-from openpyxl.utils import get_column_letter
-from openpyxl.styles import Alignment, Font
 
 # Função para verificar se um aluno já está alocado em alguma turma
 def aluno_em_turma(planilha, aluno_chave):
     abas_turmas = [sheet for sheet in planilha.sheetnames if sheet.startswith('Turma ')]
-    
+
     for turma in abas_turmas:
         aba_turma = planilha[turma]
         for row in aba_turma.iter_rows(min_row=2, max_row=aba_turma.max_row, min_col=1, max_col=3):
             nome = row[0].value
             cpf = row[1].value
             aluno_turma_chave = (nome, cpf)
-            
+
             if aluno_turma_chave == aluno_chave:
                 return True
-    
+
     return False
-
-# Função para adicionar alunos à turma
-def adicionar_alunos_a_turma(planilha, turma_destino, alunos_adicionados, quantidade):
-    aba_turma = planilha[turma_destino]
-
-    # Encontrar a primeira linha vazia após os cabeçalhos "Alunos", "CPF" e "Email"
-    primeira_linha_vazia = 2  # Começando na linha 2 para evitar os cabeçalhos
-    
-    for row in aba_turma.iter_rows(min_row=2, max_row=aba_turma.max_row, min_col=1, max_col=3):
-        if all(cell.value is None for cell in row):
-            break
-        primeira_linha_vazia += 1
-    
-    if quantidade <= 0:
-        print("Quantidade inválida. A quantidade deve ser maior que zero.")
-        return
-    
-    for _ in range(quantidade):
-        nome = f"Aluno {primeira_linha_vazia}"
-        cpf = f"CPF {primeira_linha_vazia}"
-        email = f"Email{primeira_linha_vazia}@exemplo.com"
-        aluno_chave = (nome, cpf)
-
-        # Verificar se o aluno já está alocado em alguma turma
-        if aluno_em_turma(planilha, aluno_chave):
-            print(f"O aluno {nome} com CPF {cpf} já está alocado em outra turma.")
-            continue
-
-        # Verificar se o aluno já foi adicionado a esta turma
-        aluno_completo = (nome, cpf, email)
-        if aluno_completo not in alunos_adicionados:
-            # Adicionar o aluno à turma com nome, CPF e email
-            nova_linha = [nome, cpf, email]
-            aba_turma.append(nova_linha)
-            alunos_adicionados.add(aluno_completo)
-            print(f"Aluno {nome} adicionado à {turma_destino} com sucesso.")
-            primeira_linha_vazia += 1
-
-    planilha.save('Dados Cadastrais.xlsx')
 
 # Abrir a planilha
 planilha = openpyxl.load_workbook('Dados Cadastrais.xlsx')
@@ -68,30 +27,56 @@ while True:
     print("\n1. Adicionar alunos às turmas")
     print("2. Ver alunos disponíveis e não alocados")
     print("3. Sair do programa", "\n")
-    
+
     escolha = input("Escolha uma das opções: ")
-    
+
     if escolha == '1':
         # Listar as abas de turma disponíveis
         abas_turmas = [sheet for sheet in planilha.sheetnames if sheet.startswith('Turma ')]
-        
+
         if not abas_turmas:
             print("\nNão foram encontradas abas de turma na planilha.", "\n")
         else:
             print("\nTurmas existentes:", "\n")
             for turma in abas_turmas:
                 print(turma, "\n")
-            
+
             # Solicitar ao usuário em qual turma deseja adicionar os alunos
             turma_desejada = input("Em qual turma deseja adicionar os alunos: ")
             if turma_desejada in abas_turmas:
                 quantidade_alunos = int(input("Quantos alunos deseja adicionar: "))
-                adicionar_alunos_a_turma(planilha, turma_desejada, alunos_adicionados, quantidade_alunos)
+
+                # Verificar alunos na aba "Alunos" que não foram alocados a nenhuma turma
+                aba_alunos = planilha['Alunos']
+                alunos_disponiveis = []
+
+                for row in aba_alunos.iter_rows(min_row=2, max_row=aba_alunos.max_row, min_col=1, max_col=3):
+                    nome = row[0].value
+                    cpf = row[1].value
+                    email = row[2].value
+                    aluno_chave = (nome, cpf)
+
+                    if not aluno_em_turma(planilha, aluno_chave):
+                        alunos_disponiveis.append((nome, cpf, email))
+
+                # Selecionar a quantidade desejada de alunos disponíveis
+                alunos_selecionados = alunos_disponiveis[:quantidade_alunos]
+
+                # Adicionar os alunos selecionados à turma
+                aba_turma = planilha[turma_desejada]
+                for aluno in alunos_selecionados:
+                    nome, cpf, email = aluno
+                    nova_linha = [nome, cpf, email]
+                    aba_turma.append(nova_linha)
+                    print(f"Aluno {nome} adicionado à {turma_desejada} com sucesso.")
+                    alunos_adicionados.add(aluno)
+
+                planilha.save('Dados Cadastrais.xlsx')
             else:
                 print("\nTurma não encontrada.")
     elif escolha == '2':
         alunos_nao_alocados = 0
-        
+
         # Verificar alunos na aba "Alunos" que não foram alocados a nenhuma turma
         aba_alunos = planilha['Alunos']
         for row in aba_alunos.iter_rows(min_row=2, max_row=aba_alunos.max_row, min_col=1, max_col=3):
@@ -99,10 +84,10 @@ while True:
             cpf = row[1].value
             email = row[2].value
             aluno_chave = (nome, cpf)
-            
+
             if not aluno_em_turma(planilha, aluno_chave):
                 alunos_nao_alocados += 1
-                
+
         print(f"\nAlunos disponíveis para alocação: {alunos_nao_alocados}")
     elif escolha == '3':
         break
--- Sistema/Grupos.py
+++ Sistema/Grupos.py
@@ -30,19 +30,20 @@ def criar_grupos(planilha, turma_nome, num_alunos_por_grupo):
     num_alunos = len(alunos)
     print(f"\nA turma '{turma_nome}' possui {num_alunos} alunos.")
     
-    # Verificar se é possível criar grupos com o número especificado de alunos
-    if num_alunos % num_alunos_por_grupo != 0:
-        print(f"\nO número de alunos ({num_alunos}) não é divisível pelo número de alunos por grupo ({num_alunos_por_grupo}).")
-        return
-    
     # Calcular o número total de grupos
     num_grupos = num_alunos // num_alunos_por_grupo
+    alunos_restantes = num_alunos % num_alunos_por_grupo
     
-    # Criar grupos
+    # Criar grupos com o número especificado de alunos
     grupos = []
     for i in range(num_grupos):
         grupo_alunos = alunos[i * num_alunos_por_grupo: (i + 1) * num_alunos_por_grupo]
         grupos.append(f'Grupo {i + 1} - {", ".join(grupo_alunos)}')
+    
+    # Se houver alunos restantes, criar o último grupo
+    if alunos_restantes > 0:
+        grupo_alunos = alunos[-alunos_restantes:]
+        grupos.append(f'Grupo {num_grupos + 1} - {", ".join(grupo_alunos)}')
     
     # Adicionar os grupos à coluna "Grupos" na planilha
     for i, grupo in enumerate(grupos):
@@ -50,41 +51,19 @@ def criar_grupos(planilha, turma_nome, num_alunos_por_grupo):
     
     # Salvar a planilha
     wb.save(planilha)
-    print(f"\nForam criados {num_grupos} grupos com {num_alunos_por_grupo} alunos cada na '{turma_nome}'.")
+    print(f"\nForam criados {len(grupos)} grupos com {num_alunos_por_grupo} alunos cada{' e um grupo final com ' + str(alunos_restantes) + ' alunos' if alunos_restantes > 0 else ''} na '{turma_nome}'.")
 
-# Função para contar grupos em uma turma
-def contar_grupos(planilha, turma_nome):
-    # Carregar o arquivo da planilha
+
+# Função para listar as turmas existentes
+def listar_turmas(planilha):
     wb = openpyxl.load_workbook(planilha)
-    
-    # Verificar se a turma existe
-    if turma_nome not in wb.sheetnames:
-        print(f"\nA '{turma_nome}' não foi encontrada na planilha.")
-        return
-
-    # Selecionar a aba da turma
-    sheet = wb[turma_nome]
-
-    # Obter o índice da coluna que contém o cabeçalho "Grupos"
-    coluna_grupos = None
-    for cell in sheet[1]:  # Percorre as células da primeira linha
-        if cell.value == "Grupos":
-            coluna_grupos = cell.column_letter
-            break
-    
-    if coluna_grupos is None:
-        print("Não foi encontrada uma coluna com o cabeçalho 'Grupos'.")
-        return
-
-    # Contar grupos na coluna "Grupos" da turma
-    num_grupos = sum(1 for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=coluna_grupos, max_col=coluna_grupos) if row[0].value)
-    
-    if num_grupos > 0:
-        print(f"\nA turma '{turma_nome}' possui {num_grupos} grupos.")
-        alunos_por_grupo = len(sheet.cell(row=2, column=coluna_grupos).value.split(','))  # Assumindo que todos os grupos têm o mesmo número de alunos
-        print(f"\nCada grupo possui {alunos_por_grupo} alunos.")
+    turmas = [sheet for sheet in wb.sheetnames if sheet.startswith('Turma ')]
+    if turmas:
+        print("\nTurmas existentes:", "\n")
+        for turma in turmas:
+            print(turma)
     else:
-        print(f"\nA turma '{turma_nome}' não possui grupos.")
+        print("\nNão foram encontradas abas de turma na planilha.")
 
 # Função para contar alunos em uma turma
 def contar_alunos(planilha, turma_nome):
@@ -100,19 +79,7 @@ def contar_alunos(planilha, turma_nome):
     
     # Contar alunos na turma
     num_alunos = sum(1 for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=1) if row[0].value)
-    print(f"\nA '{turma_nome}' possui {num_alunos} alunos.")
     return num_alunos  # Retornar a quantidade de alunos na turma
-
-# Função para listar as turmas existentes
-def listar_turmas(planilha):
-    wb = openpyxl.load_workbook(planilha)
-    turmas = [sheet for sheet in wb.sheetnames if sheet.startswith('Turma ')]
-    if turmas:
-        print("\nTurmas existentes:")
-        for turma in turmas:
-            print(turma)
-    else:
-        print("\nNão foram encontradas abas de turma na planilha.")
 
 # Função principal do programa
 def main():
@@ -121,9 +88,8 @@ def main():
     while True:
         print("\nOpções:")
         print("\n1. Criar grupos")
-        print("2. Ver quantos grupos têm em uma turma")
-        print("3. Listar turmas existentes")
-        print("4. Sair", "\n")
+        print("2. Listar turmas existentes")
+        print("3. Sair", "\n")
         
         escolha = input("Escolha uma opção: ")
         
@@ -132,14 +98,12 @@ def main():
             alunos_na_turma = contar_alunos(planilha, turma)  # Mostrar a quantidade de alunos na turma
             if alunos_na_turma is not None:
                 if alunos_na_turma > 0:
-                    alunos_por_grupo = int(input('Digite o número de alunos por grupo: '))
+                    print(f"\nA turma '{turma}' possui {alunos_na_turma} alunos.")
+                    alunos_por_grupo = int(input("\nDigite o número de alunos por grupo: "))
                     criar_grupos(planilha, turma, alunos_por_grupo)
         elif escolha == '2':
-            turma = input('Digite o nome da turma que deseja ver: ')
-            contar_grupos(planilha, turma)
-        elif escolha == '3':
             listar_turmas(planilha)
-        elif escolha == '4':
+        elif escolha == '3':
             print("\nSaindo do programa.", "\n")
             break
         else:
