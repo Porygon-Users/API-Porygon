de00984f6ea5b9349335ea12381b5f6b7c74709a
Isaque-BD <Isaquesc2@gmail.com>
2023-10-22 14:02:33 -0300

    teste

--- database/Relatorio.py
+++ database/Relatorio.py
@@ -1,3 +1,25 @@
-import streamlit as st #Criar dashboards
-import pandas as pd #Manipulação de dados
-import  plotly.express as px #Construção de gráficos    
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Meu Site Streamlit")
+
+with st.container():
+    st.subheader("Meu primeiro site com o Streamlit")
+    st.title("Dashboard de Contratos")
+    st.write("Informações sobre os contratos fechados pela Hash&Co ao longo de maio")
+    st.write("Quer aprender Python? [Clique aqui](https://www.hashtagtreinamentos.com/curso-python)")
+
+
+@st.cache_data
+def carregar_dados():
+    tabela = pd.read_csv("resultados.csv")
+    return tabela
+
+with st.container():
+    st.write("---")
+    qtde_dias = st.selectbox("Selecione o período", ["7D", "15D", "21D", "30D"])
+    num_dias = int(qtde_dias.replace("D", ""))
+    dados = carregar_dados()
+    dados = dados[-num_dias:]
+    st.area_chart(dados, x="Data", y="Contratos")
+
