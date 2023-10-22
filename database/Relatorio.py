f288abdc5ff550d50661a41c0f901b25b325b4b3
Isaque-BD <Isaquesc2@gmail.com>
2023-10-22 19:16:03 -0300

    teste

--- database/Relatorio.py
+++ database/Relatorio.py
@@ -8,26 +8,35 @@ df_turma1 = pd.read_excel("infodados.xlsx", sheet_name='Turma 1 (fechada)')
 df_turma2 = pd.read_excel("infodados.xlsx", sheet_name= "Turma 2")
 
 #all_spreadsheets = pd.concat(df_turma1,df_turma2)
-df_turma1["Início do Ciclo"] = pd.to_datetime(df_turma1["Início do Ciclo"])
+# df_turma1["Início do Ciclo"] = pd.to_datetime(df_turma1["Início do Ciclo"])
 
-df_turma1 = df_turma1.sort_values("Início do Ciclo")
+# df_turma1 = df_turma1.sort_values("Início do Ciclo")
 
-df_turma1["Month"] = df_turma1["Início do Ciclo"].apply(lambda x: str(x.year) + "-" + str(x.month))
+# df_turma1["Month"] = df_turma1["Início do Ciclo"].apply(lambda x: str(x.year) + "-" + str(x.month))
 
-monthy = st.sidebar.selectbox("Ciclos", df_turma1["Month"].unique())
+# monthy = st.sidebar.selectbox("Ciclos", df_turma1["Month"].unique())
 
-df_filtered = df_turma1[df_turma1["Month"] == monthy] 
+#df_filtered = df_turma1[df_turma1["Month"] == monthy] #Filtrar o gráfico com base no mês
+
+aluno = st.sidebar.selectbox("Alunos", df_turma1["Alunos"].unique())
+
+
+filtro_do_aluno = df_turma1[df_turma1["Alunos"] == aluno]
+
+alunos_notas_1 = df_turma1
+
+st.write(alunos_notas_1)
 
 col1, col2 = st.columns(2)
 col3, col4, col5 = st.columns(3)
 
-df_filtered
+nota_por_aluno = px.bar(alunos_notas_1, x = "Alunos", y = "Nota", title = "Nota por aluno")
+col1.plotly_chart(nota_por_aluno)
 
 
 
-#df_turma1 = df_turma1.sort_values(df_turma1["Início do Ciclo"])
 
-#df_turma1["Monthy"] = df_turma1["Início do Ciclo"].apply(lambda x: str(x.year) + )
+
 
 
 
