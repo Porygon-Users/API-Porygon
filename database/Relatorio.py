b8c5c956934bd3f1046f71d2168ecb231f7bb69a
Isaque-BD <Isaquesc2@gmail.com>
2023-10-26 17:49:46 -0300

    Relatorio
    Adicionado um filtro de turma
    Ele Reconhece a turma em formato de sheet e já faz um relatório com os dados contidos dessa turma

--- database/Relatorio.py
+++ database/Relatorio.py
@@ -2,57 +2,44 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(layout="wide", page_title="Relatórios gerais", page_icon=":bar_chart:")
 
-st.set_page_config(layout="wide", page_title= "Relatórios gerais",page_icon = ":bar_chart:") #configuração do layout da página
+# Ler todas as planilhas do arquivo Excel
+excel_file = pd.ExcelFile("infodados.xlsx")
+sheet_names = excel_file.sheet_names
 
-df_turma1 = pd.read_excel("infodados.xlsx", sheet_name = 'Turma 1 (fechada)')
-df_turma2 = pd.read_excel("infodados.xlsx", sheet_name = "Turma 2")
+# Filtrar apenas as planilhas que contêm "turma" no nome
+turma_sheets = [sheet_name for sheet_name in sheet_names if "turma" in sheet_name.lower()]
 
-#all_spreadsheets = pd.concat(df_turma1,df_turma2)
-# df_turma1["Início do Ciclo"] = pd.to_datetime(df_turma1["Início do Ciclo"])
+# Criar um filtro de seleção para as turmas
+selected_sheet = st.sidebar.selectbox("Selecione a turma:", turma_sheets)
 
-# df_turma1 = df_turma1.sort_values("Início do Ciclo")
-
-# df_turma1["Month"] = df_turma1["Início do Ciclo"].apply(lambda x: str(x.year) + "-" + str(x.month))
-
-# monthy = st.sidebar.selectbox("Ciclos", df_turma1["Month"].unique())
-
-#df_filtered = df_turma1[df_turma1["Month"] == monthy] #Filtrar o gráfico com base no mês
-
-
+# Ler os dados da planilha selecionada
+df_turma = pd.read_excel("infodados.xlsx", sheet_name=selected_sheet)
 
 alunos_selecionados = []
 
-st.sidebar.header("Utilize o filtro: ")
+st.sidebar.header("Utilize o filtro de alunos:")
 
 # Botão para selecionar todos os alunos
 if st.sidebar.button("Selecionar Todos"):
-    alunos_selecionados = df_turma1["Alunos"].unique()
+    alunos_selecionados = df_turma["Alunos"].unique()
 
 # Botão para remover todas as seleções
 if st.sidebar.button("Remover Todos"):
     alunos_selecionados = []
 
 # Caixa de seleção para escolher alunos individualmente
-alunos_unicos = df_turma1["Alunos"].unique()
-alunos_selecionados = st.sidebar.multiselect("Selecione os alunos: ", alunos_unicos, default=alunos_selecionados)
+alunos_unicos = df_turma["Alunos"].unique()
+alunos_selecionados = st.sidebar.multiselect("Selecione os alunos:", alunos_unicos, default=alunos_selecionados)
 
 # Filtrar o DataFrame com base nos alunos selecionados
-df_selecao = df_turma1[df_turma1["Alunos"].isin(alunos_selecionados)]
+df_selecao = df_turma[df_turma["Alunos"].isin(alunos_selecionados)]
 
 st.dataframe(df_selecao)
+
 col1, col2 = st.columns(2)
 col3, col4, col5 = st.columns(3)
-    
-nota_por_aluno = px.bar(df_selecao, x = "Alunos", y = "Nota", title = "Nota por aluno")
+
+nota_por_aluno = px.bar(df_selecao, x="Alunos", y="Nota", title="Nota por aluno")
 col1.plotly_chart(nota_por_aluno)
-
-
-
-
-
-
-
-
-
-
