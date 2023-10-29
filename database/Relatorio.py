baf0c82edbbdb112946e2b902cc814d72154367f
Isaque-BD <Isaquesc2@gmail.com>
2023-10-29 11:51:21 -0300

    Update Relatorio.py

--- database/Relatorio.py
+++ database/Relatorio.py
@@ -3,63 +3,83 @@ import pandas as pd
 import plotly.express as px
 
 
-#Configurações da página 
-st.set_page_config(layout="wide", page_title="Relatórios gerais", page_icon=":bar_chart:") 
-st.sidebar.image("pbltex.jpg",caption = "Análise de dados") #inserção da imagem
+# Função para carregar os dados do arquivo Excel
+def carregar_dados_excel():
+    excel_file = pd.ExcelFile("infodados.xlsx")
+    sheet_names = excel_file.sheet_names
+    return excel_file, sheet_names
+
+# Função para filtrar as planilhas de turma
+def filtrar_planilhas_turma(excel_file, sheet_names):
+    keywords = ["turma", "classe", "grupo"]
+    turma_sheets = [sheet_name for sheet_name in sheet_names if any(keyword in sheet_name.lower() for keyword in keywords) and "fechada" not in sheet_name.lower()]
+    return turma_sheets
+
+# Função para selecionar a planilha de turma
+def selecionar_planilha_turma(turma_sheets):
+    selected_sheet = st.sidebar.selectbox("Selecione a turma:", turma_sheets)
+    return selected_sheet
+
+# Função para aplicar o filtro de alunos
+def aplicar_filtro_alunos(df_turma):
+    alunos_unicos = df_turma["Alunos"].unique()
+    
+    # Add a "Select All" button
+    select_all_button = st.sidebar.checkbox("Todos os alunos")
+    
+    if select_all_button:
+        selected_alunos = alunos_unicos
+    else:
+        # Use multiselect if the "Selecionar Todos" checkbox is not selected
+        selected_alunos = st.sidebar.multiselect("Selecione os alunos:", alunos_unicos)
+    
+    return selected_alunos
+
+
+
+# Função para filtrar o DataFrame com base nos alunos e ciclo selecionados
+def filtrar_dataframe(df_turma, alunos_selecionados, selected_ciclo):
+    df_selecao = df_turma[df_turma["Alunos"].isin(alunos_selecionados)][["Alunos", selected_ciclo, "Média", "Professores", "Início do Curso", "Fim do Curso"]]
+    return df_selecao
+
+# Função para criar gráficos
+def criar_graficos(df_selecao, selected_ciclo):
+    col1, col2 = st.columns(2)
+    
+    nota_por_aluno = px.bar(df_selecao, x="Alunos", y=selected_ciclo, title=f"Nota do {selected_ciclo} por aluno")
+    col1.plotly_chart(nota_por_aluno)
+
+    media_por_aluno = px.bar(df_selecao, x="Alunos", y="Média", title="Média por aluno")
+    col2.plotly_chart(media_por_aluno)
+
+# Configurações da página
+st.set_page_config(layout="wide", page_title="Relatórios gerais", page_icon=":bar_chart:")
+st.sidebar.image("pbltex.jpg", caption="Análise de dados")
 st.title("Relatório das Turmas")
 
-# Ler todas as planilhas do arquivo Excel
-excel_file = pd.ExcelFile("infodados.xlsx")
-sheet_names = excel_file.sheet_names
+# Carregar os dados do arquivo Excel
+excel_file, sheet_names = carregar_dados_excel()
 
-# Defina palavras-chave relacionadas às turmas
-keywords = ["turma", "classe", "grupo"]  # Adicione outras palavras-chave, se necessário
+# Filtrar as planilhas de turma
+turma_sheets = filtrar_planilhas_turma(excel_file, sheet_names)
 
-# Filtrar as planilhas que contêm pelo menos uma palavra-chave e não contêm "fechada"
-turma_sheets = [sheet_name for sheet_name in sheet_names if any(keyword in sheet_name.lower() for keyword in keywords) and "fechada" not in sheet_name.lower()]
-
-# Criar um filtro de seleção para as turmas
-selected_sheet = st.sidebar.selectbox("Selecione a turma:", turma_sheets)
+# Selecionar a planilha de turma
+selected_sheet = selecionar_planilha_turma(turma_sheets)
 
 # Ler os dados da planilha selecionada
 df_turma = pd.read_excel("infodados.xlsx", sheet_name=selected_sheet)
 
-alunos_selecionados = []    
-
-# Adicione um seletor de ciclo
+# Adicionar um seletor de ciclo
 colunas_ciclo = [coluna for coluna in df_turma.columns if coluna.lower().startswith("ciclo")]
 selected_ciclo = st.sidebar.selectbox("Selecione o ciclo:", colunas_ciclo)
 
-st.sidebar.header("Utilize o filtro de alunos:")
-# Inicialmente, definimos a variável de seleção de alunos como todos os alunos
-alunos_selecionados = df_turma["Alunos"].unique()
+# Aplicar o filtro de alunos
+alunos_selecionados = aplicar_filtro_alunos(df_turma)
 
-# Verifique se o botão "Remover Todos" foi clicado
-if st.sidebar.button("Remover Todos"):
-    alunos_selecionados = []
+# Filtrar o DataFrame com base nos alunos e ciclo selecionados
+df_selecao = filtrar_dataframe(df_turma, alunos_selecionados, selected_ciclo)
 
-# Verifique se o botão "Selecionar Todos" foi clicado
-if st.sidebar.button("Selecionar Todos"):
-    alunos_selecionados = df_turma["Alunos"].unique()
-
-# Caixa de seleção para escolher alunos individualmente
-alunos_unicos = df_turma["Alunos"].unique()
-alunos_selecionados = st.sidebar.multiselect("Selecione os alunos:", alunos_unicos, default=alunos_selecionados)
-
-
-# Filtrar o DataFrame com base nos alunos selecionados e no ciclo selecionado
-df_selecao = df_turma[df_turma["Alunos"].isin(alunos_selecionados)][["Alunos", selected_ciclo, "Média","Professores","Início do Curso","Fim do Curso"]]
+# Criar gráficos
+criar_graficos(df_selecao, selected_ciclo)
 
 st.dataframe(df_selecao)
-
-col1, col2 = st.columns(2)
-col3, col4, col5 = st.columns(3)
-
-# Criar um gráfico de barras para as notas do ciclo selecionado
-nota_por_aluno = px.bar(df_selecao, x="Alunos", y=selected_ciclo, title=f"Nota do {selected_ciclo} por aluno")
-col1.plotly_chart(nota_por_aluno)
-
-# Criar um gráfico de colunas para a média de cada aluno
-media_por_aluno = px.bar(df_selecao, x="Alunos", y="Média", title="Média por aluno")
-col2.plotly_chart(media_por_aluno)
-
